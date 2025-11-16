11a71b42d700989408af5fa3d1a255083bdbaede
psj-labs <sungjinpark2202@gmail.com>
2025-11-16 18:06:44 +0900

    Add comments for IP and MAC address variables
    Added comments to clarify IP and MAC address variables.

--- seed-attack-scripts.py/MAC/spoof-arp.py
+++ seed-attack-scripts.py/MAC/spoof-arp.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 from scapy.all import *
 
-IP_V       = "10.9.0.5"
-MAC_V_real = "02:42:0a:09:00:05"
+IP_V       = "10.9.0.5" # Sender IP Address
+MAC_V_real = "02:42:0a:09:00:05" # Sender MAC Address
 
 IP_T       = "10.9.0.99"
 MAC_T_fake = "aa:bb:cc:dd:ee:ff"
